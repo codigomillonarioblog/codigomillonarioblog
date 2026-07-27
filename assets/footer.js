e85bb572a8d3184556817b00ada6efc9e243e130
CODIGOMILLONARIO <interactivegpt@gmail.com>
2026-07-28 00:39:25 +0200

    Add files via upload

--- assets/footer.js
+++ assets/footer.js
@@ -66,10 +66,10 @@ document.addEventListener("DOMContentLoaded", function () {
       <div class="footer-col">
         <h4>Información Legal</h4>
         <ul>
-          <li><a href="/informacion-legal#nuestra-filosofia">Nuestra filosofía</a></li>
           <li><a href="/informacion-legal#sobre-el-blog">Sobre el Blog</a></li>
           <li><a href="/informacion-legal#politica-privacidad">Política de Privacidad</a></li>
           <li><a href="/informacion-legal#terminos-condiciones">Términos y Condiciones</a></li>
+          <li><a href="/informacion-legal#nuestra-filosofia">Nuestra filosofía</a></li>
           <li><a href="/informacion-legal#aviso-legal">Aviso Legal</a></li>
         </ul>
       </div>
