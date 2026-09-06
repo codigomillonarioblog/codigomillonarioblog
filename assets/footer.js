77f7526901521eb77be089db438119cf75164d17
CODIGOMILLONARIO <interactivegpt@gmail.com>
2026-09-06 21:07:36 +0200

    Add files via upload

--- assets/footer.js
+++ assets/footer.js
@@ -57,16 +57,16 @@ document.addEventListener("DOMContentLoaded", function () {
         <ul>
           <li><a href="/educacion-financiera.html">Artículos</a></li>
           <li><a href="/herramientas.html">Calculadoras</a></li>
-          <li><a href="/opiniones.html">Historias</a></li>
-          <li><a href="/blog/negocios/index.html">Emprendimiento</a></li>
           <li><a href="/blog/inversiones/index.html">Guías de inversión</a></li>
+          <li><a href="/blog/negocios/index.html">Emprendimiento</a></li>
+          <li><a href="/opiniones.html">Historias</a></li>
         </ul>
       </div>
 
       <div class="footer-col">
         <h4>Información Legal</h4>
         <ul>
-          <li><a href="/informacion-legal#sobre-el-blog">Sobre el Blog</a></li>
+          <li><a href="/informacion-legal#sobre-el-blog">Sobre el Medio</a></li>
           <li><a href="/informacion-legal#politica-privacidad">Política de Privacidad</a></li>
           <li><a href="/informacion-legal#terminos-condiciones">Términos y Condiciones</a></li>
           <li><a href="/informacion-legal#nuestra-filosofia">Nuestra filosofía</a></li>
@@ -78,6 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <h4>Contacto</h4>
         <ul>
           <li><a href="/contacto.html">Soporte</a></li>
+          <li>codigomillonarioblog.@gamil.com</li>
         </ul>
       </div>
 
